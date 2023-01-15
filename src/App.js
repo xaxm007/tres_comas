fd0fcbbea2957af72694ca726996f337eea5b7e0
Reshav <thapareshav50@gmail.com>
2023-01-15 10:17:11 +0545

    Committed

--- src/App.js
+++ src/App.js
@@ -2,30 +2,23 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // Importing pages from Pages component
 import Home from "./Pages/Home/Home"
-import AboutUs from './Pages/AboutUs/Aboutus'
+
 import Buy from './Pages/Buy/Buy'
 import Sell from './Pages/Sell/Sell'
 import Rent from './Pages/Rent/Rent'
-import Contacts from './Pages/Contacts/Contacts'
+
 
 const App = () => {
   return (
     <>
-    
-
-      <Home />
-      <AboutUs />
-      <Contacts />
       <BrowserRouter>
         <Routes>
+          <Route exact path = "/" element = {<Home />} />
           <Route exact path = "/buy" element = {< Buy />} />
           <Route exact path = "/sell" element = {<Sell />} />
           <Route exact path = '/rent' element = {<Rent/>} />
-
         </Routes>
       </BrowserRouter>
-      
-      
     </>
   )
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css'
 
+
 const Home = () => {
   return (
     <>
@@ -30,6 +31,7 @@ const Home = () => {
             </div>
 
         </section>
+        
     </>
   )
 }
